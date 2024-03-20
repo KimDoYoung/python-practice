5e596dea48aab66c7904d2ff1fd394908c8570c8
KimDoYoung <kdy987@gmail.com>
2024-03-20 17:09:15 +0900

    0320

--- merian/backend/main.py
+++ merian/backend/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os
 from backend.app.api.endpoints.user import router as user_router
@@ -10,6 +11,15 @@ load_dotenv()  # 환경변수 로드
 app = FastAPI()
 
 app.include_router(user_router)
+
+# "static" 폴더를 "/static" 경로에 마운트합니다.
+# 이 예제에서는 프로젝트 루트 디렉토리에 "static" 폴더가 있고,
+# 그 안에 "js", "css", "images" 등의 서브 디렉토리가 있다고 가정합니다.
+# 현재 파일(main.py)의 위치를 기준으로 상대 경로를 구성합니다.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+static_files_path = os.path.join(BASE_DIR, 'frontend', 'static')
+
+app.mount("/static", StaticFiles(directory=static_files_path), name="static")
 
 # 템플릿 환경 설정
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
