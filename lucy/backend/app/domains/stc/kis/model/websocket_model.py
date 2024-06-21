1d28c7a2316fad2dbb748a9c9ba3dde4f668874e
KimDoYoung <kdy987@gmail.com>
2024-06-21 16:53:50 +0900

    0621

--- lucy/backend/app/domains/stc/kis/model/websocket_model.py
+++ lucy/backend/app/domains/stc/kis/model/websocket_model.py
@@ -1,6 +1,6 @@
 import json
-from typing import Type, TypeVar
-from pydantic import BaseModel, ValidationError
+from typing import List, Type, TypeVar
+from pydantic import BaseModel, Field, ValidationError
 
 T = TypeVar('T', bound='KisWsResponseBase')
 
@@ -40,3 +40,67 @@ class KisWsResponse(KisWsResponseBase):
 
     def isPingPong(self) -> bool:
         return self.header.tr_id == 'PINGPONG'
+    
+#
+# 호가 실시간데이터
+#     
+
+class CostQty(BaseModel):
+    가격: int
+    잔량: int
+
+class H0STASP0(BaseModel):
+    단축종목코드: str
+    영업시간: str
+    시간구분코드: str
+    매도호가배열: List[CostQty]
+    매수호가배열: List[CostQty]
+    # 매도호가잔량배열: List[int]
+    # 매수호가잔량배열: List[int]
+    
+    총매도호가_잔량: int = Field(..., alias='TOTAL_ASKP_RSQN')
+    총매수호가_잔량: int = Field(..., alias='TOTAL_BIDP_RSQN')
+    시간외_총매도호가_잔량: int = Field(..., alias='OVTM_TOTAL_ASKP_RSQN')
+    시간외_총매수호가_잔량: int = Field(..., alias='OVTM_TOTAL_BIDP_RSQN')
+    예상_체결가: int = Field(..., alias='ANTC_CNPR')
+    예상_체결량: int = Field(..., alias='ANTC_CNQN')
+    예상_거래량: int = Field(..., alias='ANTC_VOL')
+    예상_체결_대비: int = Field(..., alias='ANTC_CNTG_VRSS')
+    예상_체결_대비부호: str = Field(..., alias='ANTC_CNTG_VRSS_SIGN')
+    예상_체결_전일대비율: float = Field(..., alias='ANTC_CNTG_PRDY_CTRT')
+    누적_거래량: int = Field(..., alias='ACML_VOL')
+    총매도호가_잔량_증감: int = Field(..., alias='TOTAL_ASKP_RSQN_ICDC')
+    총매수호가_잔량_증감: int = Field(..., alias='TOTAL_BIDP_RSQN_ICDC')
+    시간외_총매도호가_잔량_증감: int = Field(..., alias='OVTM_TOTAL_ASKP_ICDC')
+    시간외_총매수호가_잔량_증감: int = Field(..., alias='OVTM_TOTAL_BIDP_ICDC')
+    주식매매_구분코드: str = Field(..., alias='STCK_DEAL_CLS_CODE')
+    
+    @classmethod
+    def from_text(cls, text: str) -> "H0STASP0":
+        recvvalue = text.strip().split('^')
+        매도호가 = [CostQty(가격=int(recvvalue[i]), 잔량=int(recvvalue[23 + i])) for i in range(3, 13)]
+        매수호가 = [CostQty(가격=int(recvvalue[13 + i]), 잔량=int(recvvalue[33 + i])) for i in range(10)]
+        
+        return cls(
+            단축종목코드=recvvalue[0],
+            영업시간=recvvalue[1],
+            시간구분코드=recvvalue[2],
+            매도호가배열=매도호가,
+            매도호가배열=매수호가,
+            총매도호가_잔량=int(recvvalue[43]),
+            총매도호가_잔량_증감=int(recvvalue[54]),
+            총매수호가_잔량=int(recvvalue[44]),
+            총매수호가_잔량_증감=int(recvvalue[55]),
+            시간외_총매도호가_잔량=int(recvvalue[45]),
+            시간외_총매수호가_잔량=int(recvvalue[46]),
+            시간외_총매도호가_잔량_증감=int(recvvalue[56]),
+            시간외_총매수호가_잔량_증감=int(recvvalue[57]),
+            예상_체결가=int(recvvalue[47]),
+            예상_체결량=int(recvvalue[48]),
+            예상_거래량=int(recvvalue[49]),
+            예상_체결_대비=int(recvvalue[50]),
+            예상_체결_대비부호=recvvalue[51],
+            예상_체결_전일대비율=float(recvvalue[52]),
+            누적_거래량=int(recvvalue[53]),
+            주식매매_구분코드=recvvalue[58]
+        )    
--- lucy/backend/tests/domains/stc/kis/model/test_websocket_model.py
+++ lucy/backend/tests/domains/stc/kis/model/test_websocket_model.py
@@ -1,6 +1,6 @@
 
 import pytest
-from backend.app.domains.stc.kis.model.websocket_model import KisWsResponse
+from backend.app.domains.stc.kis.model.websocket_model import H0STASP0, KisWsResponse
 
 
 def test_create():
@@ -23,4 +23,39 @@ def test_create():
 
     with pytest.raises(ValueError):
         kis_ws_response_model = KisWsResponse.from_json_str(wrong_json_data)
-    
+
+def test_hoga():
+    # 데이터 예시
+    data_str = """
+    005930^153948^0^80100^80200^80300^80400^80500^80600^80700^80800^80900^81000^80000^79900^79800^79700^79600^79500^79400^79300^79200^79100^77855^73175^72845^706^4642^68925^67828^50635^85021^157328^481698^238211^173635^103434^135417^163796^50590^43877^34519^43445^658960^1468622^9635^88360^0^0^3292594^-81600^5^-100.00^16873825^0^0^-19^0^0
+    """
+
+    # Pydantic 모델로 변환
+    quote_data = H0STASP0.from_text(data_str)
+
+    # 데이터 출력
+    print(f"유가증권 단축 종목코드 [{quote_data.단축종목코드}]")
+    print(f"영업시간 [{quote_data.영업시간}] 시간구분코드 [{quote_data.시간구분코드}]")
+    print("======================================")
+    for i in range(10):
+        print(f"매도호가{10-i:02} [{quote_data.매도호가[i].가격}]    잔량{10-i:02} [{quote_data.매도호가[i].잔량}]")
+    print("--------------------------------------")
+    for i in range(10):
+        print(f"매수호가{10-i:02} [{quote_data.매수호가[i].가격}]    잔량{10-i:02} [{quote_data.매수호가[i].잔량}]")
+    print("======================================")
+    print(f"총매도호가 잔량        [{quote_data.총매도호가_잔량}]")
+    print(f"총매도호가 잔량 증감   [{quote_data.총매도호가_잔량_증감}]")
+    print(f"총매수호가 잔량        [{quote_data.총매수호가_잔량}]")
+    print(f"총매수호가 잔량 증감   [{quote_data.총매수호가_잔량_증감}]")
+    print(f"시간외 총매도호가 잔량 [{quote_data.시간외_총매도호가_잔량}]")
+    print(f"시간외 총매수호가 잔량 [{quote_data.시간외_총매수호가_잔량}]")
+    print(f"시간외 총매도호가 잔량 증감 [{quote_data.시간외_총매도호가_잔량_증감}]")
+    print(f"시간외 총매수호가 잔량 증감 [{quote_data.시간외_총매수호가_잔량_증감}]")
+    print(f"예상 체결가            [{quote_data.예상_체결가}]")
+    print(f"예상 체결량            [{quote_data.예상_체결량}]")
+    print(f"예상 거래량            [{quote_data.예상_거래량}]")
+    print(f"예상체결 대비          [{quote_data.예상_체결_대비}]")
+    print(f"예상체결 대비부호      [{quote_data.예상_체결_대비부호}]")
+    print(f"예상체결 전일대비율    [{quote_data.예상_체결_전일대비율}]")
+    print(f"누적거래량             [{quote_data.누적_거래량}]")
+    print(f"주식매매 구분코드      [{quote_data.주식매매_구분코드}]")
