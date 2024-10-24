1b77a1fa80bcbfd9682999885f4d32f61e4bac4e
KimDoYoung <kdy987@gmail.com>
2024-10-24 13:42:06 +0900

    1024

--- ipo-scheduler/backend/app/core/logger.py
+++ ipo-scheduler/backend/app/core/logger.py
@@ -6,18 +6,42 @@ def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(config.LOG_LEVEL)
     LOG_FILE = config.LOG_FILE
-    if not logger.handlers:
-        # 매일 자정에 로그 파일을 회전, 최대 7개의 파일 보관
-        # 파일의 최대 크기는 예시로 10MB로 설정하였습니다. 필요에 따라 조절하십시오.
-        file_handler = ConcurrentRotatingFileHandler(LOG_FILE, "a", 10*1024*1024, 7)
-        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
-        
-        if config.PROFILE_NAME == "local":
-            # 콘솔에도 로그 메시지 출력
-            console_handler = logging.StreamHandler()
-            console_handler.setFormatter(formatter)
-            logger.addHandler(console_handler)
+    
+    # 기존 핸들러를 명확히 제거한 후 추가
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    # 로그 파일 핸들러 추가
+    file_handler = ConcurrentRotatingFileHandler(LOG_FILE, "a", 10*1024*1024, 7)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    # 로컬 환경일 경우 콘솔 출력 핸들러 추가
+    if config.PROFILE_NAME == "local":
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
 
     return logger
+
+# def get_logger(name):
+#     from backend.app.core.config import config
+#     logger = logging.getLogger(name)
+#     logger.setLevel(config.LOG_LEVEL)
+#     LOG_FILE = config.LOG_FILE
+#     if not logger.handlers:
+#         # 매일 자정에 로그 파일을 회전, 최대 7개의 파일 보관
+#         # 파일의 최대 크기는 예시로 10MB로 설정하였습니다. 필요에 따라 조절하십시오.
+#         file_handler = ConcurrentRotatingFileHandler(LOG_FILE, "a", 10*1024*1024, 7)
+#         formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+#         file_handler.setFormatter(formatter)
+#         logger.addHandler(file_handler)
+        
+#         if config.PROFILE_NAME == "local":
+#             # 콘솔에도 로그 메시지 출력
+#             console_handler = logging.StreamHandler()
+#             console_handler.setFormatter(formatter)
+#             logger.addHandler(console_handler)
+
+#     return logger
