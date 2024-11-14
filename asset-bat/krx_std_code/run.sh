4927b92b4b8d38441eb74a8c57358e22a4b9af85
Kim, Do Young <kdy987@gmail.com>
2024-11-14 17:03:31 +0900

    1114

--- asset-bat/krx_std_code/run.sh
+++ asset-bat/krx_std_code/run.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
-
-# 가상 환경 활성화
-source ../env/bin/activate
-
-# 날짜 형식 정의 (yyyy_mm_dd)
 current_date=$(date +"%Y_%m_%d")
+PRG_NAME="krx_std_code"
+PROJECT_ROOT=$(realpath "$(dirname "$0")/..")
 
 # 로그 디렉터리 생성
-mkdir -p ../log/fetch_ipo_data
+mkdir -p "$PROJECT_ROOT/logs/$PRG_NAME"
+log_file="$PROJECT_ROOT/logs/$PRG_NAME/${PRG_NAME}_${current_date}.log"
 
-# 로그 파일 경로 설정 (날짜 포함)
-log_file="../log/fetch_ipo_data/fetch_ipo_data_${current_date}.log"
+# 운영 체제 확인
+OS_TYPE=$(uname)
 
-# Python 스크립트 실행 및 로그 파일에 append 모드로 저장
-python main.py >> "$log_file" 2>&1
+echo "Project Root: [$PROJECT_ROOT]"
+echo "OS TYPE: $OS_TYPE"
+echo "Log File: $log_file"
 
-# 가상 환경 비활성화
-deactivate
+if [[ "$OS_TYPE" == "Linux" || "$OS_TYPE" == "Darwin" ]]; then
+    # 가상 환경 활성화 (Linux/macOS)
+    export ASSET_BAT_MODE="real"
+    export PYTHONPATH="$PROJECT_ROOT:$PYTHONPATH"
+    source $PROJECT_ROOT/env/bin/activate
+
+    # Python 스크립트 실행 및 로그 파일에 저장
+    $PROJECT_ROOT/env/Scripts/python "$PROJECT_ROOT/$PRG_NAME/main.py" >> "$log_file" 2>&1
+    
+    # 가상 환경 비활성화
+    deactivate
+else
+    echo "Unsupported Operating System: $OS_TYPE"
+    exit 1
+fi
