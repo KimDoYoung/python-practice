b7d6308d182b4fa5605a0144021152d74b487b8e
KimDoYoung <kdy987@gmail.com>
2024-12-07 09:36:01 +0900

    1207

--- kalpadb-api/app/api/v1/endpoints/essay_routes.py
+++ kalpadb-api/app/api/v1/endpoints/essay_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.database import get_session
-from app.domain.essay.essay_schema import EssayListResponse, EssayResponse, EssayUpsertRequest
+from app.domain.essay.essay_schema import EssayListRequest, EssayListResponse, EssayResponse, EssayUpsertRequest
 from app.domain.essay.essay_service import EssayService
 
 router = APIRouter()
@@ -25,7 +25,7 @@ async def delete_essay(id:int, db: AsyncSession = Depends(get_session)):
     return await service.delete_essay(id)
 
 @router.get("/essays", response_model=EssayListResponse, summary="에세이 Page 조회")
-async def get_essays(db: AsyncSession = Depends(get_session)):
+async def get_essays(request: EssayListRequest, db: AsyncSession = Depends(get_session)):
     ''' 에세이 1 page 조회 '''
     service = EssayService(db)
-    return await service.get_essays()
+    return await service.get_essays(request)
